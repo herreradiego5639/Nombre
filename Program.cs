d7bf999e0eea50adf5a4b6843d41bcd0f3ca9c0e
dherrera <dherrera@DIEGOHERRERA>
2023-02-23 17:32:15 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+Console.WriteLine("Ingrese su nombre: ");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Hola," + nombre);
+
+Console.ReadKey();
+
+
